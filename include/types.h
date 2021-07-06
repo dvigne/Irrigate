9da2921d9f70adb98773a320141de444aca51add
Derick Vigne <derickvigne@me.com>
2021-07-06 00:10:23 -0400

    Added Global Constants

--- include/types.h
+++ include/types.h
@@ -6,4 +6,7 @@
 
 #define STATUS_LED 16
 
+const String statusClasses[] = { "text-primary", "text-warning" };
+const String statusMessages[] = { "Everything is Good", "Needs Your Attention" };
+
 enum _errorFlags { NO_ERRORS, TIME_NOT_SET, JSON_PARSING_ERROR };
